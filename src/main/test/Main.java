84939321f51f1d32f8f4dd20a10a63fd78accf04
nibnait <tb178271@alibaba-inc.com>
2020-01-09 13:51:05 +0800

    add ListTest

--- src/main/test/Main.java
+++ src/main/test/Main.java
@@ -1,5 +1,11 @@
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.base.Functions;
+import com.google.common.collect.Lists;
 import junit.framework.TestCase;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by nibnait on 2020-01-08
