5585b0ea746e64b3b409e50ab7b548e41762db07
nibnait <w@tianbin.org>
2020-03-02 14:52:29 +0800

    update

--- src/main/test/Main.java
+++ src/main/test/Main.java
@@ -1,11 +1,5 @@
-import com.alibaba.fastjson.JSONObject;
-import com.google.common.base.Functions;
-import com.google.common.collect.Lists;
 import junit.framework.TestCase;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by nibnait on 2020-01-08
@@ -15,6 +9,9 @@ public class Main extends TestCase {
     @Test
     public void testCase() {
 
+        System.out.println("岗位---姓名--体温\n菜品制作员--熊贵芳--36.2\n打包员--饶强--36");
+
     }
+
 
 }
